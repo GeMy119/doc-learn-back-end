88cedf14a882db29711223c4771a596e404d34bb
GeMy119 <teba7033@gmail.com>
2024-04-26 03:46:02 +0300

    update middlewares

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -3,14 +3,19 @@ import jwt from "jsonwebtoken";
 const authenticateTokenCookie = (req, res, next) => {
   console.log("Inside authentication middleware");
 
-  const token = req.headers.token;
+  const token = req.headers.authorization;
   console.log("Token from headers in authenticateTokenCookie function: ", token);
 
   if (!token) {
     return res.status(401).json({ message: "Please provide a cookie token" });
   }
 
-  jwt.verify(token, "secret_key", (err, decoded) => {
+  let tokenValue = token;
+  if (token.startsWith("Bearer ")) {
+    tokenValue = token.slice(7); // Remove 'Bearer ' prefix
+  }
+
+  jwt.verify(tokenValue, "secret_key", (err, decoded) => {
     if (err) {
       console.log("Error inside handleVerificationResult: ", err);
       return res.status(403).json({ message: "Cookie token verification failed", error: err });
@@ -19,7 +24,6 @@ const authenticateTokenCookie = (req, res, next) => {
     req.student = decoded; // Decoded payload is set as the student property of the req object
     console.log("Decoded payload inside authentication middleware: ", decoded);
 
-    res.status(201).json({ message: "Successful Cookie Token Verification" });
     next(); // Move to the next middleware or route handler
   });
 };
